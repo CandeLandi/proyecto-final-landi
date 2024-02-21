388470a3c67a802a324624bb329b7f5ec9fb7294
Candela Landi <landicandela01@gmail.com>
2024-02-21 15:09:35 -0300

    add db.json

--- src/app/layouts/auth/auth.service.spect.ts
+++ src/app/layouts/auth/auth.service.spect.ts
@@ -1,31 +1,50 @@
 import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 import { MOCK_USER } from './auth.service';
-import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 
 describe('Servicio de autenticación', () => {
-  let service: AuthService;
+  let authService: AuthService;
   let router: Router;
-  let snackBar: MatSnackBar;
+
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule, MatSnackBarModule],
-      providers: [AuthService, localStorage],
+      providers: [AuthService],
+      imports: [
+        ToastrModule.forRoot(),
+      ]
     });
+    authService = TestBed.inject(AuthService)
+
+    it('AuthService debe estar definido', () => {
+      expect(authService).toBeTruthy();
+    })
 
     describe('login', () => {
       it('debería navegar a cursos y almacenar el usuario al iniciar sesión', () => {
         const datosLogin = { email: MOCK_USER.email, password: MOCK_USER.password };
-        service.login(datosLogin);
+        const navigateSpy = spyOn(router, 'navigate');
+        const storeSpy = spyOn(localStorage, 'setItem');
+
+        authService.login(datosLogin);
+
+        expect(navigateSpy).toHaveBeenCalledWith(['/cursos']);
+        expect(storeSpy).toHaveBeenCalledWith('user', JSON.stringify(MOCK_USER));
       });
+
 
       it('debería abrir un toast con un mensaje de error al iniciar sesión incorrectamente', () => {
         const datosLoginIncorrectos = { email: 'correo@incorrecto.com', password: 'contraseñaIncorrecta' };
-        service.login(datosLoginIncorrectos);
+        const toastrSpy = spyOn(TestBed.inject(ToastrService), 'error');
+
+        authService.login(datosLoginIncorrectos);
+
+        expect(toastrSpy).toHaveBeenCalledWith('Error al iniciar sesión', 'Usuario o contraseña incorrectos');
       });
+
     });
   })
 })
